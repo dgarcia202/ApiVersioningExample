3f421a3416fd2be84f77b44f52f226fba48def46
Diego García <wft2000@gmail.com>
2021-09-18 16:32:39 +0200

    first commit

---
+++ ApiVersioningExample/Filters/PublishRequestedApiVersionFilter.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ApiVersioningExample.Filters
+{
+    public class PublishRequestedApiVersionFilter : IActionFilter
+    {
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            var httpContext = context.HttpContext;
+
+            var v = httpContext.GetRequestedApiVersion();
+            if (v != null)
+                httpContext.Response.Headers.Add("Content-Version", v.ToString());
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+
+        }
+    }
+}
---
+++ ApiVersioningExample/Startup.cs
@@ -0,0 +1,58 @@
+using ApiVersioningExample.Filters;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+namespace ApiVersioningExample
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers(config =>
+            {
+                config.Filters.Add<PublishRequestedApiVersionFilter>();
+            });
+
+            services.AddApiVersioning(config =>
+            {
+                config.DefaultApiVersion = new ApiVersion(2, 0);
+                config.AssumeDefaultVersionWhenUnspecified = true;
+                config.ReportApiVersions = true;
+                config.ApiVersionReader = new HeaderApiVersionReader("Accept-Version");
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
